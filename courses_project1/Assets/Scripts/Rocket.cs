67b1e0a597d58ad7b0039a664019fd128eacebcc
sofi_blonde <101343357+sofiblonde@users.noreply.github.com>
2023-11-17 16:01:53 +0300

    less2
    rotation, songs

--- courses_project1/Assets/Scripts/Rocket.cs
+++ courses_project1/Assets/Scripts/Rocket.cs
@@ -1,38 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     // выполняется при старте
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    //выполняется при раскадровке
+    //выполняется покадрово
     void Update()
     {
-        ProcessInput(){ }
+        Launch();
+        Rotation();
     }
 
-    void ProcessInput() 
+    private void Launch()
     {
-        if(Input.GetKey(KeyCode.Space))
+               if(Input.GetKey(KeyCode.Space))
         {
+            //print("зажали пробел");
 
+            rigidBody.AddRelativeForce(Vector3.up);
+
+            if(!audioSource.isPlaying) 
+                audioSource.Play();
         }
+        else 
+        { 
+            audioSource.Pause();
+        }  
+    }
+
+    void Rotation() 
+    {
+
 
         if(Input.GetKey(KeyCode.A)) 
         {
             //print("поворот влево");
+            transform.Rotate(Vector3.forward);
         }
 
         else if(Input.GetKey(KeyCode.D)) 
         {
             //print("поворот влево");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
