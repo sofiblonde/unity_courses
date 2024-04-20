ce18f5165383e483cbda1ebcd023b7b78ac2c9c5
sofi_blonde <101343357+sofiblonde@users.noreply.github.com>
2024-04-20 15:56:21 +0300

    moveobjects
    cyclic movement of an object

---
+++ courses_project1/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+
+public class MoveObject : MonoBehaviour
+{
+
+    [SerializeField] Vector3 movePosition;
+    [SerializeField] [Range(0,1)] float moveProgress;
+    [SerializeField] float moveSpeed;
+    Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveProgress = Mathf.PingPong(Time.time*moveSpeed, 1);
+
+        Vector3 offset = movePosition * moveProgress;
+       transform.position = startPosition + offset;
+    }
+}
